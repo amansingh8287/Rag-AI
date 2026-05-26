ea2d634c2a40868a522b457411f139886702ff86
work.amansingh12@gmail.com <work.amansingh12@gmail.com>
2026-05-26 18:02:45 +0530

    frontend deployed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ export default function AdvancedRagChatbot() {
     try {
       // API CALL
 
-      const response = await fetch("http://localhost:5000/chat", {
+      const response = await fetch(" https://ragbase-backend.onrender.com", {
         method: "POST",
 
         headers: {
